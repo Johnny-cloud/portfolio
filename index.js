729e3a215fb59e52ac5d3e18c67af71a63ce4a43
John Kipruto <johnkipruto157@gmail.com>
2024-03-20 16:40:42 +0300

    Add event listeners to nav-links to collapse toggle bar upon clicking

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-let contactForm = document.getElementById("contactForm")
-
-contactForm.addEventListener('submit', event => {
-
-    event.preventDefault()
-
-    let contactName = document.getElementById("contactName")
-    let contactEmail = document.getElementById("contactEmail")
-    let contactMessage = document.getElementById("contactMessage")
-
-    let recipient = "johnkipruto157@gmail.com"
-    let message = contactMessage.value 
-    let subject = "Getting In Touch"
-
-    let mailToLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
-    
-    window.location.href = mailToLink
-
-    contactName.value = contactEmail.value = contactMessage.value = " "
-
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = bootstrap.Collapse.getOrCreateInstance(menuToggle, {toggle: false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { 
+        if(bsCollapse._isShown()){
+            bsCollapse.hide()
+        }
+    })
 })
