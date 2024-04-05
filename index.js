aa8aa5813d0ab37fde499eba0f3df98ac871d858
John Kipruto <johnkipruto157@gmail.com>
2024-04-05 15:45:54 +0300

    Update hero background

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const navLinks = document.querySelectorAll('.nav-item')
+
 const menuToggle = document.getElementById('navbarSupportedContent')
+
 const bsCollapse = bootstrap.Collapse.getOrCreateInstance(menuToggle, {toggle: false})
+
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { 
         if(bsCollapse._isShown()){
